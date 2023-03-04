e1ac05362658b5c4a4e83ad7ad47d6aa0b211efc
Shantanu <pawarshantanu58@gmail.com>
2023-03-04 20:50:55 +0530

    added implementation for diff.java

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class diff {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        System.out.println("The sum is "+(a-b));
+        System.out.println("Minus");
+    }
+}
